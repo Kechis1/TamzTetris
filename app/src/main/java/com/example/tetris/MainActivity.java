473f8eed45d1ca14daf6bd8f1d986cadd3f024de
Kechis1 <danielbill95@gmail.com>
2018-11-04 18:08:29 +0100

    Removed exit button, added grid

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.tetris;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    TextView TVExit;
     TextView TVVolume;
     TextView TVCog;
     TextView TVPlay;
@@ -18,10 +18,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TVExit = findViewById(R.id.tvExit);
         TVVolume = findViewById(R.id.tvVolume);
         TVCog = findViewById(R.id.tvCog);
         TVPlay = findViewById(R.id.tvPlay);
         TVMedal = findViewById(R.id.tvMedal);
+
+        TVPlay.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MainActivity.this.startActivity(new Intent(MainActivity.this, GameActivity.class));
+                    }
+                }
+        );
+
     }
 }
